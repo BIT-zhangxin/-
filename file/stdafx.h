a78af370208542bbb43516a7499f098e761113aa
BIT-zhangxin <37972354+BIT-zhangxin@users.noreply.github.com>
2018-06-18 00:27:04 +0800

    Create stdafx.h

---
+++ file/stdafx.h
@@ -0,0 +1,22 @@
+// stdafx.h : 标准系统包含文件的包含文件，
+// 或是经常使用但不常更改的
+// 特定于项目的包含文件
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <cstdlib>
+#include <tchar.h>
+#include <iostream>
+#include <string.h>
+#include <cstring>
+#include <ctime>
+using namespace std;
+#include "Fraction.h"
+#include "CreateExpression.h"
+#include "Solve.h"
+
+// TODO: 在此处引用程序需要的其他头文件
